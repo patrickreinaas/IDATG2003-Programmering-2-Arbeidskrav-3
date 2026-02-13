767f48cde91b4d4495b63a8e5ce92c5d6c591a48
Patrick <patrir@stud.ntnu.no>
2026-02-13 19:02:50 +0100

    feat: add interface class TextCommand with a method to execute a text

---
+++ src/main/java/text/TextCommand.java
@@ -0,0 +1,27 @@
+/*
+ * TODO:
+ *
+ *
+ * file     TextCommand.java
+ * @author  Patrick Reinås
+ * @version 0.0.1
+ * @since   13.02.2026
+ */
+
+
+package text;
+
+
+/*
+ * TextCommand class
+ */
+public interface TextCommand {
+
+
+    /*
+     * Method to execute a string
+     *
+     * @param text - The string
+     */
+    String execute(String text);
+}
